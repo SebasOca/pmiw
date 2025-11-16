497c2a80dd1f947523329705da5b2ccd05e8859a
SebasOca <129012466+SebasOca@users.noreply.github.com>
2025-11-16 00:28:49 -0300

    Add files via upload

--- tpfinalparte1/tpfinalparte1.js
+++ tpfinalparte1/tpfinalparte1.js
@@ -1,6 +1,9 @@
+//Enlace: https://youtu.be/mcLmOw8h-3Q
+
 let fuente;
 let textos;
 let soundtrack;
+
 let cartel1;
 let cartel2;
 let imagenesFondo = [];
@@ -17,7 +20,7 @@ function preload() {
   textos = loadStrings('/data/textos.txt');
   soundFormats ('mp3');
   soundtrack = loadSound("data/soundtrack.mp3");
-  
+
   cartel1 = loadImage("data/cartel1.png");
   cartel2 = loadImage("data/cartel2.png");
   for (let i=0; i<=cant; i++) {
@@ -46,13 +49,14 @@ function mouseClicked() {
   if (numFondo === 0) {
     if (mouseX >= 270 && mouseX <= 370 && mouseY >= 345 && mouseY <= 390) {
       numFondo++;
+      soundtrack.play();
       lineaTexto = numFondo-1;
     }
   } else if (numFondo > 0) {
     if (mouseX >= 510 && mouseX <= 600 && mouseY >= 365 && mouseY <= 400) {
       numFondo++;
       lineaTexto = numFondo-1;
-      soundtrack.play();
+
       if (numFondo === 9 || numFondo === 13 || numFondo === 16) {
         numFondo = 0;
         lineaTexto = numFondo-1;
